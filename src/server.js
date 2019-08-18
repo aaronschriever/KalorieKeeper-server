31d061e1919d0f6551ef4ac6af52ad8748b6da88
Aaron Schriever <aaron.schriever@gmail.com>
2019-08-18 19:02:12 +0930

    fix dbname variable naming

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const settings = require('../config/app.settings.json');
 
 app.listen(settings.port), () => {
-    console.llog('app listening on port ' + settings.port)
+    console.log('app listening on port ' + settings.port)
 };
